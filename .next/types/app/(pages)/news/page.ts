eae6b24f1c1d6000ee84ca00b3c6181a5d75fc3a
GiaBao0227 <nguyengiabao272@gmail.com>
2025-02-25 20:34:12 +0700

    upload source

--- .next/types/app/(pages)/news/page.ts
+++ .next/types/app/(pages)/news/page.ts
@@ -1,4 +1,4 @@
-// File: D:\CycberSoft\Frontend\Buoi_15\Bai_Tap\Archive\src\app\(pages)\news\page.tsx
+// File: D:\CycberSoft\Frontend\Buoi_15\Bai_Tap\Demo_NextJs\src\app\(pages)\news\page.tsx
 import * as entry from '../../../../../src/app/(pages)/news/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: D:\CycberSoft\Frontend\Buoi_15\Bai_Tap\Archive\src\app\page.tsx
+// File: D:\CycberSoft\Frontend\Buoi_15\Bai_Tap\Demo_NextJs\src\app\page.tsx
 import * as entry from '../../../src/app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
